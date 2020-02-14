0d2f9fc2883c36849b18d9331434e6f7bd99551d
Roland Lammel <rl@neotel.at>
2020-02-14 14:38:08 +0100

    Provide metrics.WritePrometheusSet to write using a dedicated metric set

--- metrics.go
+++ metrics.go
@@ -45,3 +45,22 @@ func WritePrometheus(w io.Writer, exposeProcessMetrics bool) {
 		writeProcessMetrics(w)
 	}
 }
+
+// WritePrometheusMetricSet will write all metrics registered in the provided set in Prometheus format to w.
+//
+// If exposeProcessMetrics is true, then various `go_*` and `process_*` metrics
+// are exposed for the current process.
+//
+// The WritePrometheusMetricSet func is usually called inside "/metrics" handler:
+//
+//     http.HandleFunc("/metrics", func(w http.ResponseWriter, req *http.Request) {
+//         metrics.WritePrometheusSet(set, w, true)
+//     })
+//
+func WritePrometheusMetricSet(set *Set, w io.Writer, exposeProcessMetrics bool) {
+	set.WritePrometheus(w)
+	if exposeProcessMetrics {
+		writeGoMetrics(w)
+		writeProcessMetrics(w)
+	}
+}
